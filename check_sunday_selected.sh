23cb5a46bd5e744b904b69f1e12cc0bc7c8d9b45
limweiyau <weiyau@live.com>
2025-08-20 13:12:11 +0800

    Finished assignment

--- check_sunday_selected.sh
+++ check_sunday_selected.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Checks if the Sunday option is selected in week1exercise1.html
-grep -qi "<option value=['\"]Sunday['\"] selected" week1exercise1.html
+# Checks if the Sunday option is selected in wk1exercise1.html
+grep -qi "<option value=['\"]Sunday['\"] selected" wk1exercise1.html
